cf925186b28a11ead2ed6e1405414abbc02d72c8
ajlopez <webmaster@ajlopez.com>
2014-06-29 13:35:17 -0300

    Load composite JSON

--- test/load.js
+++ test/load.js
@@ -9,3 +9,15 @@ exports['Load simple JSON'] = function (test) {
     test.equal(simple.title, 'Simple JSON');
     process.chdir('..');
 }
+
+exports['Load composite JSON'] = function (test) {
+    process.chdir('test');
+    var compose = lj.load('compose');
+    
+    test.ok(compose);
+    test.equal(compose.author, "ajlopez");
+    test.ok(compose.project);
+    test.equal(typeof compose.project, "object");
+    test.equal(compose.project.title, 'Simple JSON');
+    process.chdir('..');
+}

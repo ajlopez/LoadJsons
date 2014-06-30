c530cca61f0e0e9750854f6ae558dc0aba2dfcaa
ajlopez <webmaster@ajlopez.com>
2014-06-30 08:47:32 -0300

    Load from another directory

--- test/load.js
+++ test/load.js
@@ -10,6 +10,13 @@ exports['Load simple JSON'] = function (test) {
     process.chdir('..');
 }
 
+exports['Load simple JSON from directory'] = function (test) {
+    var simple = lj.load('test/simple3');
+    
+    test.ok(simple);
+    test.equal(simple.title, 'Simple JSON');
+}
+
 exports['Load simple JSON with explicit extension'] = function (test) {
     process.chdir('test');
     var simple = lj.load('simple.json');
@@ -29,4 +36,14 @@ exports['Load composite JSON'] = function (test) {
     test.equal(typeof compose.project, "object");
     test.equal(compose.project.title, 'Simple JSON');
     process.chdir('..');
+}
+
+exports['Load composite JSON from directory'] = function (test) {
+    var compose = lj.load('test/compose2');
+    
+    test.ok(compose);
+    test.equal(compose.author, "ajlopez");
+    test.ok(compose.project);
+    test.equal(typeof compose.project, "object");
+    test.equal(compose.project.title, 'Simple JSON');
 }
